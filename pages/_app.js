1f630760a95012d81c6a78bf2c6e940ad86bc1ed
Basit Qayoom Chowdhary <basitqayoomchowdhary@Basit-12905.local>
2023-08-22 11:41:37 +0530

    google meta tag added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import {useEffect} from 'react'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   useEffect(()=>{
     import('lit-pw/dist/bundle')
   },[])
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+      <meta name="google-site-verification" content="ukpf0_hN5N4IQLTrvLSH2xd9kaWmk6E_TYRkjjNTI64" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
